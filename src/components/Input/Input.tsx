41fab0ef213352a03cba7850b2001eed12529604
Shaqs0 <hhgfhhhggb@gmail.com>
2024-05-10 10:08:39 +0300

    update input component

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,12 +1,23 @@
+import { useState } from 'react';
 import Button from '../Button/Button';
 import styles from './Input.module.css';
 
 export function Input() {
+	const [inputValue, setInputValue] = useState('');
+
+	const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setInputValue(event.target.value);
+	};
+
+	const handleButtonClick = () => {
+
+		console.log('Button clicked');
+	};
 
 	return <div>
 		<div className={styles['input_container']}>
-			<input className= {styles['input']} placeholder='Введите ссылку, которую нужно сократить'></input>
-			<Button appearence='small'>Сократить</Button>
+			<input type='text' value={inputValue} onChange={handleInputChange} className= {styles['input']} placeholder='Введите ссылку, которую нужно сократить'></input>
+			<Button onClick={handleButtonClick} disabled={!inputValue} appearence='small'>Сократить</Button>
 		</div>
 	</div>;
 }
