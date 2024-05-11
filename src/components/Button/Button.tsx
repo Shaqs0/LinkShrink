8145e0303c72d34b1443097e24c00a96872ba11e
Shaqs0 <hhgfhhhggb@gmail.com>
2024-05-11 23:01:13 +0300

    update_MainPage

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -6,6 +6,7 @@ import cn from 'classnames';
 function Button({ children, className, appearence = 'small', ...props }: ButtonProps) {
 	return (
 		<button className={cn(styles['button'], styles['accent'], className, {
+			[styles['very_small']]: appearence === 'very_small',
 			[styles['small']]: appearence === 'small',
 			[styles['big']]: appearence === 'big'
 		})} {...props}>{children}</button>
